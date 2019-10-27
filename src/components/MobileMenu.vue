dd7e514ece1e83d5604ba19fc6691f2d76aef8a0
Alexander Schetnikov <go.sanyaaa@gmail.com>
2019-10-27 12:51:47 +0700

    +(MobileMenu) Добавлен тестовый компонент для мобильного меню

---
+++ src/components/MobileMenu.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+  p Меню
+</template>
+
+<script>
+    export default {
+        name: "MobileMenu"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import Vue from "vue"
 import App from "@/App.vue"
+import MobileMenu from "@/components/MobileMenu.vue";
 import VueTheMask from 'vue-the-mask'
 
 Vue.use(VueTheMask)
 
 var vm = new Vue({
-    components: {App},
+    components: {App, MobileMenu},
 })
 
 vm.$mount("#app")
